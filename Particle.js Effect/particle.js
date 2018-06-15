232a93a3cb153c9bde164343f0d105555effb53a
Bishal Goswami <38840313+goswamibishal@users.noreply.github.com>
2018-06-15 10:42:25 +0530

    Add files via upload

---
+++ Particle.js Effect/particle.js
@@ -0,0 +1,23 @@
+$(".kk").typed({
+
+  strings: [ "Jquery effect","Partical.js" ],
+    startDelay: 1000,
+  typeSpeed:100,
+    typeSpeed: 100,
+ 
+
+  // time before typing starts
+
+  startDelay: 110,
+ 
+  // backspacing speed
+  backSpeed: 110,
+
+  // shuffle the strings
+  shuffle: false,
+
+  // time before backspacing
+  backDelay: 100
+ 
+
+}); 
---
+++ Twenty Twenty Effect in Jquery/project.js
@@ -0,0 +1,31 @@
+
+
+$(".ggg").typed({
+
+  strings: [ "Developer","Web Developer" ],
+    startDelay: 1000,
+  typeSpeed:100,
+    typeSpeed: 110,
+ 
+
+  // time before typing starts
+
+  startDelay: 110,
+ 
+  // backspacing speed
+  backSpeed: 110,
+
+  // shuffle the strings
+  shuffle: false,
+
+  // time before backspacing
+  backDelay: 500
+ 
+
+}); 
+
+$(window).load(function() {
+  $(".twentytwenty-container").twentytwenty();
+});
+
+
